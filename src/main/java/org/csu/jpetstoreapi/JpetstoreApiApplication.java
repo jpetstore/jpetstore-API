d49b1d1a4783a1ccf7baa63579ccb79964121747
Rosekey <1207710425@qq.com>
2023-04-29 17:03:14 +0800

    first

---
+++ src/main/java/org/csu/jpetstoreapi/JpetstoreApiApplication.java
@@ -0,0 +1,13 @@
+package org.csu.jpetstoreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpetstoreApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpetstoreApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/csu/jpetstoreapi/JpetstoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.csu.jpetstoreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpetstoreApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
