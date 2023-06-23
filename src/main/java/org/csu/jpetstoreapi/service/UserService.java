7dd59e1137652ec598b5831f28b8e23e178b982f
zengfanshuo <2742276404@qq.com>
2023-06-23 13:10:15 +0800

    统一用user（2）

--- src/main/java/org/csu/jpetstoreapi/service/UserService.java
+++ src/main/java/org/csu/jpetstoreapi/service/UserService.java
@@ -2,10 +2,12 @@ package org.csu.jpetstoreapi.service;
 
 import org.csu.jpetstoreapi.common.CommonResponse;
 import org.csu.jpetstoreapi.entity.User;
+import org.csu.jpetstoreapi.entity.UserInfo;
 
 public interface UserService {
 
     CommonResponse<User> login(String id, String password);
+//    CommonResponse<UserInfo> login(String id, String password);
 
     CommonResponse<User> insertUser(User user);
     CommonResponse<User> getAccountByUsernameAndPassword(String username,String password);
@@ -15,5 +17,7 @@ public interface UserService {
 
     CommonResponse<User> updateUserById(User user);
     CommonResponse<User> updateUserById_2(User user);
+
+    CommonResponse<User> updateUserExceptPwd(User user);
 
 }
